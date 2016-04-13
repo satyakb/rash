5065a31a97c23694391e9be7dfe75896540150b8
Satya Bodduluri <skbodduluri@gmail.com>
2016-04-13 19:03:58 -0400

    echo loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut buf = String::new();
+        match io::stdin().read_line(&mut buf) {
+            Err(e) => {
+                panic!("error: {}", e);
+            },
+            Ok(_) => {
+                print!("{}", buf);
+            }
+        }
+    }
 }
